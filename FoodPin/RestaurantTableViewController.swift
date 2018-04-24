1a7d68b47c0beaa98f811bf32fb1cfbf9c40e7f6
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2018-04-24 00:00:28 -0400

    adding alert controller using table view delegate method didSelectRowAt

--- FoodPin/RestaurantTableViewController.swift
+++ FoodPin/RestaurantTableViewController.swift
@@ -49,6 +49,22 @@ class RestaurantTableViewController: UITableViewController {
         return cell
     }
     
+    // MARK: Table View Delegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // create an option menu as an action sheet
+        let optionMenu = UIAlertController(title: nil, message: "What do you want to do?", preferredStyle: .actionSheet)
+        
+        // add actions to the menu
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        optionMenu.addAction(cancelAction)
+        
+        // display the menu
+        present(optionMenu, animated: true, completion: nil)
+        
+    }
+    
 
     /*
     // Override to support conditional editing of the table view.
