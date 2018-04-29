4b1cb280ad8cf2e3ddcfcb6c0792cb3767c57860
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2018-04-29 15:13:08 -0400

    cleaning up detail view controller; turning off the largeTitleDisplayMode

--- FoodPin/Controller/RestaurantDetailViewController.swift
+++ FoodPin/Controller/RestaurantDetailViewController.swift
@@ -4,31 +4,14 @@ class RestaurantDetailViewController: UIViewController {
     
     //MARK: Properties
     
-    
-    @IBOutlet weak var restaurantImageView: UIImageView!
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var locationLabel: UILabel!
-    
     var restaurant = Restaurant()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // makes navigation bar narrow
+        navigationItem.largeTitleDisplayMode = .never
 
-        restaurantImageView.image = UIImage(named: restaurant.image)
-        nameLabel.text = restaurant.name
-        locationLabel.text = restaurant.location
     }
-
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
