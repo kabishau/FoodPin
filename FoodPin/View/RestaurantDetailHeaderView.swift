52b0f147a60adb3fa7dfbbfe9fd203a1a5f862fc
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2018-05-01 00:23:36 -0400

    Header View Class and its properties have been added

---
+++ FoodPin/View/RestaurantDetailHeaderView.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class RestaurantDetailHeaderView: UIView {
+
+    //MARK: Properties
+    @IBOutlet var headerImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var heartImageView: UIImageView!
+
+}
