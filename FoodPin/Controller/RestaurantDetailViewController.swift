b1bc947a4f9bbf8d7f9c423d2bac12d2ada032a4
user139405 <user139405@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-05-02 16:49:06 -0400

    adding code to display data in the previously designed table view cell prototypes

--- FoodPin/Controller/RestaurantDetailViewController.swift
+++ FoodPin/Controller/RestaurantDetailViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class RestaurantDetailViewController: UIViewController {
+class RestaurantDetailViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     //MARK: Properties
     
@@ -9,8 +9,13 @@ class RestaurantDetailViewController: UIViewController {
     
     var restaurant = Restaurant()
     
+    //MARK: Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //tableView.delegate = self
+        //tableView.dataSource = self
         
         // makes navigation bar narrow
         navigationItem.largeTitleDisplayMode = .never
@@ -22,5 +27,43 @@ class RestaurantDetailViewController: UIViewController {
         headerView.heartImageView.isHidden = (restaurant.isVisited) ? false : true
 
     }
+    
+    //MARK: Data Source Methods
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.row {
+            
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailIconTextCell.self), for: indexPath) as! RestaurantDetailIconTextCell
+            cell.iconImageView.image = UIImage(named: "phone")
+            cell.shortTextLabel.text = restaurant.phone
+            return cell
+        
+        case 1:
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailIconTextCell.self), for: indexPath) as! RestaurantDetailIconTextCell
+        cell.iconImageView.image = UIImage(named: "map")
+        cell.shortTextLabel.text = restaurant.location
+        return cell
+        
+        case 2:
+            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailTextCell.self), for: indexPath) as! RestaurantDetailTextCell
+            cell.descriptionLabel.text = restaurant.description
+            return cell
+        
+        default:
+            fatalError("Failed to instantiate the table view cell for detail view controller")
+            
+        }
+    }
+    
+    
 
 }
