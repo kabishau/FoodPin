6a649b4e66b05fa714160ee15c57717e6718a793
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2018-05-01 21:50:06 -0400

    making the corner of type label be rounded

--- FoodPin/View/RestaurantDetailHeaderView.swift
+++ FoodPin/View/RestaurantDetailHeaderView.swift
@@ -9,7 +9,12 @@ class RestaurantDetailHeaderView: UIView {
             nameLabel.numberOfLines = 0
         }
     }
-    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel! {
+        didSet {
+            typeLabel.layer.cornerRadius = 5.0
+            typeLabel.layer.masksToBounds = true
+        }
+    }
     @IBOutlet var heartImageView: UIImageView!
 
 }
