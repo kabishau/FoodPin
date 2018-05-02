26cb1f40744d935e1f0b2eeb58c7b6c948d21a40
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2018-05-01 22:59:00 -0400

    custom class for RestaurantDetailIconTextCell and connections to IB

---
+++ FoodPin/View/RestaurantDetailIconTextCell.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+class RestaurantDetailIconTextCell: UITableViewCell {
+    
+    //MARK: Properties
+    
+    @IBOutlet var iconImageView: UIImageView!
+    @IBOutlet var shortTextLabel: UILabel! {
+        didSet {
+            shortTextLabel.numberOfLines = 0
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
