a257fb7006413c1e97f58e49f7a068899f1d2ff7
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2018-05-09 21:59:35 -0400

    modal segue has been created between rate button and review view controller; prepare(for:sender) has been modify for new identifier

--- FoodPin/Controller/RestaurantDetailViewController.swift
+++ FoodPin/Controller/RestaurantDetailViewController.swift
@@ -56,6 +56,9 @@ class RestaurantDetailViewController: UIViewController, UITableViewDataSource, U
         if segue.identifier == "showMap" {
             let destinationController = segue.destination as! MapViewController
             destinationController.restaurant = restaurant
+        } else if segue.identifier == "showReview" {
+            let destinationController = segue.destination as! ReviewViewController
+            destinationController.restaurant = restaurant
         }
     }
     
