9a4e9c86c4ed21dc199b84afc2ac6bf87d93b0eb
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2018-05-11 01:23:02 -0400

    custom text field class

---
+++ FoodPin/View/RoundedTextField.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+class RoundedTextField: UITextField {
+
+    let padding = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+    // call every time when the text field is laid out
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        self.layer.cornerRadius = 5.0
+        self.layer.masksToBounds = true
+    }
+
+}
