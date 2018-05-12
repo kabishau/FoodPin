54bf55db747b07f629311e6b26baadb9af74cabd
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2018-05-11 22:45:09 -0400

    adding new class for new restaurant screen; setting tag to text fields and text view

---
+++ FoodPin/Controller/NewRestaurantController.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+class NewRestaurantController: UITableViewController, UITextFieldDelegate {
+    
+    //MARK: Properties
+    
+    @IBOutlet var nameTextField: RoundedTextField! {
+        didSet {
+            nameTextField.tag = 1
+            nameTextField.becomeFirstResponder()
+            nameTextField.delegate = self
+        }
+    }
+    
+    @IBOutlet var typeTextField: RoundedTextField! {
+        didSet {
+            typeTextField.tag = 2
+            typeTextField.delegate = self
+        }
+    }
+    
+    @IBOutlet var addressTextField: RoundedTextField! {
+        didSet {
+            addressTextField.tag = 3
+            addressTextField.delegate = self
+        }
+    }
+    
+    @IBOutlet var phoneTextField: RoundedTextField! {
+        didSet {
+            phoneTextField.tag = 4
+            phoneTextField.delegate = self
+        }
+    }
+    
+    @IBOutlet var descriptionTextView: RoundedTextField! {
+        didSet {
+            descriptionTextView.tag = 5
+            descriptionTextView.layer.cornerRadius = 5.0
+            descriptionTextView.layer.masksToBounds = true
+        }
+    }
+    
+    //MARK: View Controller Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
